1254da4cb590c57f7ea9692a59ed44c3b252a824
Ayman <ayman4001@hotmail.com>
2016-12-06 16:11:47 -0700

    first action listener

---
+++ src/Colors.java
@@ -0,0 +1,41 @@
+/**
+ * Created by Sumatra Group on 12/6/2016.
+ */
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class Colors {
+
+
+
+    public class ColorJPanel extends JPanel
+ {
+         // draw rectangles and Strings in different colors
+         @Override
+         public void paintComponent(Graphics g)
+         {
+             super.paintComponent(g);
+             this.setBackground(Color.WHITE);
+             // set new drawing color using integers
+             g.drawString("Current RGB: " + g.getColor() , 130, 40);
+             // set new drawing color using floats
+
+             g.fillRect(15, 50, 100, 20);
+             g.drawString("Current RGB: " + g.getColor(), 130, 65);
+
+             // set new drawing color using static Color objects
+
+             g.fillRect(15, 75, 100, 20);
+             g.drawString("Current RGB: " + g.getColor(), 130, 90);
+
+             // display individual RGB values
+
+             g.fillRect(15, 100, 100, 20);
+
+             g.drawString("RGB values: " + g.getColor() + ", " + 36 + ", " + g.getColor(), 130, 115);
+             }
+         } // end class ColorJPanel
+
+}
--- src/Paint.java
+++ src/Paint.java
@@ -1,7 +1,7 @@
 /**
  * Created by Sumatra Group on 12/5/2016.
  */
-import oracle.jrockit.jfr.JFR;
+
 
 import javax.swing.*;
 import java.awt.event.*;
@@ -10,55 +10,76 @@ import java.awt.geom.*;
 import java.util.*;
 
 
-public class Paint extends JFrame{
+public class Paint extends JFrame {
 
-    JButton myBrushButton,myEllipseButton,myLineButton, myFillButton;
-    int currentAction=1;
-    Color stokeColor=Color.black, fillColor=Color.red;
+    JButton myRectangleButton, myEllipseButton, myLineButton, myStokeButton, myFillButton;
+    Color strokeColor = Color.BLACK, fillColor = Color.RED;
 
 
     public static void main(String[] args) {
         System.out.println("Welcome to Paint, Enjoy it!");
         new Paint();
 
-
     }
 
     public Paint(){
 
-        this.setSize(500,500);
+        this.setSize(500, 500);
         this.setTitle("Sumatra Paint");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel paintPanel = new JPanel();
-        myBrushButton = new JButton("Brush");
-        myBrushButton.setSize(50,50);
-
-        myEllipseButton = new JButton("Ellipse");
-        myEllipseButton.setSize(50,50);
-        myLineButton = new JButton("Line");
-        myLineButton.setSize(50,50);
+        myRectangleButton = new JButton("Rectangle");
+        myRectangleButton.setSize(50, 50);
+        myStokeButton = new JButton("Stoke");
+        myStokeButton.setSize(50, 50);
         myFillButton = new JButton("Fill");
-        myFillButton.setSize(50,50);
+        myFillButton.setSize(50, 50);
+        myEllipseButton = new JButton("Ellipse");
+        myEllipseButton.setSize(50, 50);
+        myLineButton = new JButton("Line");
+        myLineButton.setSize(50, 50);
+
 
         Box myBox = Box.createHorizontalBox();
-        myBox.setBackground(Color.yellow);
-        myBox.add(myBrushButton);
+        myBox.add(myStokeButton);
+        myBox.add(myFillButton);
+        myBox.add(myRectangleButton);
+
+        myBox.setBackground(Color.YELLOW);
         myBox.add(myEllipseButton);
         myBox.add(myLineButton);
-        myBox.add(myFillButton);
-        paintPanel.add(myBox);
 
 
-
-        this.add(paintPanel);
+        paintPanel.add(myBox, BorderLayout.SOUTH);
+        paintPanel.add(new DrawingBoard(), BorderLayout.CENTER);
+        this.add(paintPanel, BorderLayout.SOUTH);
         this.setVisible(true);
 
-        myBrushButton.addActionListener(new ActionListener() {
+        myStokeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("This button brush");
+                    strokeColor = JColorChooser.showDialog(null,"Choose a stroke color",Color.RED);
             }
+
+        });
+
+        myFillButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                fillColor = JColorChooser.showDialog(null,"Choose a fill color",Color.BLUE);
+            }
+        });
+
+        myRectangleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                drawRectangle(10, 10, 20, 20);
+                }
+
+
+
         });
 
         myEllipseButton.addActionListener(new ActionListener() {
@@ -79,9 +100,62 @@ public class Paint extends JFrame{
         myLineButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                Shape myLine =new Line2D.Float(10,10,90,80);
 
             }
         });
     }
 
-}
+
+
+
+    public class DrawingBoard extends JComponent {
+        ArrayList<Shape> shapes = new ArrayList<Shape>();
+        ArrayList<Color> fillColor = new ArrayList<Color>();
+        ArrayList<Shape> stokeColor = new ArrayList<Shape>();
+        Point startP, endP;
+
+        public DrawingBoard() {
+            this.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent mouseEvent) {
+                    startP = new Point(mouseEvent.getX(), mouseEvent.getY());
+                    endP = startP;
+                    repaint();
+                }
+                public void MouseReleased(MouseEvent mouseEvent)
+                {
+                   Shape myRect = drawRectangle(5,5,5,5);
+                   shapes.add(myRect);
+                    repaint();
+                }
+
+                public void draw(Graphics g) {
+                    Graphics2D graphSettings = (Graphics2D) g;
+                    graphSettings.setStroke(new BasicStroke(2));
+
+
+                    for (Shape s : shapes) {
+                        graphSettings.draw(s);
+
+                    }
+                }
+
+
+            });
+
+            }
+        }
+
+
+        private Rectangle2D.Float drawRectangle(int x1, int y1, int x2, int y2) {
+
+            return new Rectangle2D.Float(x1,y1,x2,y2);
+
+        }
+
+   }
+
+
+
+
